2abd1989b097bf92c52da4d40a5ac7e41912112d
sarkarusama <sheikhusama8017@gmail.com>
2025-02-02 20:42:21 +0500

    remove typewriter

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -2,7 +2,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
-// import Typewriter from "typewriter-effect";
+// import typewriter from "react-simple-typewriter";
 import heroImage from "@/public/profile/profile2.jpg"
 const Hero = () => {
   return (
